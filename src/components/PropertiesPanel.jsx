6d155f75a8f27c8950546a7bce461f2ad443dc0a
nastenack <natsenack@gmail.com>
2025-10-10 17:56:55 +0200

    Reverse conditional rendering: show 'Bordures & Coins' when 'Aucune bordure' is activated

--- src/components/PropertiesPanel.jsx
+++ src/components/PropertiesPanel.jsx
@@ -508,7 +508,7 @@ const PropertiesPanel = React.memo(({
               />
             )}
 
-            {localProperties.borderWidth > 0 && (
+            {localProperties.borderWidth === 0 && (
               <div className="properties-group">
                 <h4>🔲 Bordures & Coins</h4>
 
