591bfcfb04fdad8b6ec3af1c5a646aa8c6e16f38
nastenack <natsenack@gmail.com>
2025-10-11 20:24:24 +0200

    Déploiement automatique - 2025-10-11 20:24:24

--- includes/config.php
+++ includes/config.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 
 // Version du plugin
 if (!defined('PDF_BUILDER_PRO_VERSION')) {
-    define('PDF_BUILDER_PRO_VERSION', '1.0.1');
+    define('PDF_BUILDER_PRO_VERSION', '1.0.2');
 }
 
 // Répertoire du plugin
