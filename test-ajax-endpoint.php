20cfd139384c8bd0d5fcd0176d341f0019bd534b
nastenack <natsenack@gmail.com>
2025-10-13 15:34:53 +0200

    Déploiement automatique - 2025-10-13 15:34:53

--- test-ajax-endpoint.php
+++ test-ajax-endpoint.php
@@ -5,8 +5,21 @@
 
 define('PDF_GENERATOR_TEST_MODE', true);
 
+// Définir les fonctions WordPress manquantes pour le test standalone
+if (!function_exists('plugin_dir_path')) {
+    function plugin_dir_path($file) {
+        return dirname($file) . '/';
+    }
+}
+
+if (!function_exists('plugin_dir_url')) {
+    function plugin_dir_url($file) {
+        return 'http://localhost/wp-content/plugins/wp-pdf-builder-pro/';
+    }
+}
+
 // Simuler une requête AJAX comme le ferait le frontend React
-$_POST['action'] = 'generate_pdf_from_elements';
+$_POST['action'] = 'pdf_builder_generate_preview';
 $_POST['elements'] = json_encode([
     [
         'id' => 'test-text-1',
@@ -74,6 +87,6 @@ if (!function_exists('wp_send_json')) {
 }
 
 // Inclure le fichier AJAX
-require_once __DIR__ . '/includes/pdf-generator-ajax.php';
+require_once __DIR__ . '/includes/pdf-preview-generator.php';
 
 echo "Test terminé.\n";
