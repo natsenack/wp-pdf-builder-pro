5d598b0549d0c5f6a942a50ccc7af52653a17cb0
nastenack <natsenack@gmail.com>
2025-10-14 04:03:08 +0200

    Déploiement automatique - 2025-10-14 04:03:08

--- src/components/CanvasElement.jsx
+++ src/components/CanvasElement.jsx
@@ -16,6 +16,7 @@ export const CanvasElement = ({
   dragAndDrop
 }) => {
   const elementRef = useRef(null);
+  const canvasRectRef = useRef(null);
 
   const resize = useResize({
     onElementResize: (newRect) => {
