4ec11e9b99a6db94b199ae65e0efeb2da15184c0
nastenack <natsenack@gmail.com>
2025-10-15 22:06:54 +0200

    Déploiement automatique - 2025-10-15 22:06:54

--- src/components/PDFCanvasEditor.jsx
+++ src/components/PDFCanvasEditor.jsx
@@ -806,7 +806,7 @@ export const PDFCanvasEditor = ({ options }) => {
           canvasHeight={canvasState.canvasHeight}
           ajaxurl={window.pdfBuilderAjax?.ajaxurl}
           pdfBuilderNonce={window.pdfBuilderAjax?.nonce}
-          useServerPreview={true}
+          useServerPreview={false}
           onOpenPDFModal={(pdfUrl) => {
             setPdfModalUrl(pdfUrl);
             setShowPDFModal(true);
