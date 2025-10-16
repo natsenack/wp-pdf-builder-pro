27cbd3050db46a35f72907614e4cecc8011e3515
nastenack <natsenack@gmail.com>
2025-10-16 04:31:26 +0200

    Déploiement automatique - 2025-10-16 04:31:26

--- src/PDFCanvasEditor.jsx
+++ src/PDFCanvasEditor.jsx
@@ -1,11 +1,1 @@
 import React from 'react';
-
-const handleShowGridChange = () => {
-    console.log('handleShowGridChange triggered');
-    // TODO: Implémenter la logique de changement d'affichage de la grille
-};
-
-// Expose handleShowGridChange globally
-if (typeof window !== 'undefined') {
-  window.handleShowGridChange = handleShowGridChange;
-}
