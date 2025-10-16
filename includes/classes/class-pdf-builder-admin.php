7423667e791e62643ffb32abe97c68a5f8db28f9
nastenack <natsenack@gmail.com>
2025-10-16 23:42:33 +0200

    Déploiement automatique - 2025-10-16 23:42:33

--- includes/classes/class-pdf-builder-admin.php
+++ includes/classes/class-pdf-builder-admin.php
@@ -1380,7 +1380,15 @@ class PDF_Builder_Admin {
         ');
 
         // Scripts JavaScript - VERSION ULTRA FORCEE
-        wp_enqueue_script('pdf-builder-react-vendors', PDF_BUILDER_PRO_ASSETS_URL . 'js/dist/react-vendors.js', ['jquery'], '1.0.0_force_' . microtime(true), true);
+        // CHARGER REACT AVANT NOS SCRIPTS !
+        wp_enqueue_script('react', includes_url('js/dist/vendor/react.js'), [], '18.2.0', true);
+        wp_enqueue_script('react-dom', includes_url('js/dist/vendor/react-dom.js'), ['react'], '18.2.0', true);
+
+        // Exposer React globalement pour nos bundles
+        wp_add_inline_script('react', 'window.React = window.React || React;');
+        wp_add_inline_script('react-dom', 'window.ReactDOM = window.ReactDOM || ReactDOM;');
+
+        wp_enqueue_script('pdf-builder-react-vendors', PDF_BUILDER_PRO_ASSETS_URL . 'js/dist/react-vendors.js', ['react', 'react-dom'], '1.0.0_force_' . microtime(true), true);
         wp_enqueue_script('pdf-builder-admin-v3', PDF_BUILDER_PRO_ASSETS_URL . 'js/dist/pdf-builder-admin.js', ['jquery', 'wp-api', 'pdf-builder-react-vendors'], '8.0.0_force_' . microtime(true), true);
 
         // DEBUG: Vérifier que le script est enqueued
