9a03388d84541ae5f81b5475bf1c765a8f4c810b
nastenack <natsenack@gmail.com>
2025-10-16 23:58:27 +0200

    Déploiement automatique - 2025-10-16 23:58:27

--- src/components/PDFCanvasEditor.jsx
+++ src/components/PDFCanvasEditor.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react';
 import { CanvasElement } from './CanvasElement';
 import { useDragAndDrop } from '../hooks/useDragAndDrop';
 import { Toolbar } from './Toolbar';
