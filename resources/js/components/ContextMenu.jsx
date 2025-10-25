cf3ec629533a043ead4a5ac7a7d770791ce2a529
PDF Builder Pro <pdf-builder-pro@local.dev>
2025-10-25 21:31:18 +0200

    Déploiement automatique - 2025-10-25 21:31:18
    - 5 fichiers déployés

--- resources/js/components/ContextMenu.jsx
+++ resources/js/components/ContextMenu.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const ContextMenu = ({ menu, onAction, isAnimating = false, onClose }) => {
   const menuRef = useRef(null);
