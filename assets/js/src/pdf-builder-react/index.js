ef1cd6cbe94c6f35e1961b60b61c2ee16cba3a9d
PDF Builder Pro <pdf-builder-pro@local.dev>
2025-10-27 22:45:28 +0100

    fix: Drag-drop FTP deploy - 2025-10-27 22:45:28

--- assets/js/src/pdf-builder-react/index.js
+++ assets/js/src/pdf-builder-react/index.js
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Header } from './components/header/Header.tsx';
-import './components/header/Header.css';
 
 // État de l'application
 let currentTemplate = null;
