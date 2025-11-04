4c592ec6dd1afbed45617292d09dd11e6e4ee977
PDF Builder Pro <pdf-builder-pro@local.dev>
2025-11-04 16:14:19 +0100

    fix: Drag-drop FTP deploy - 2025-11-04 16:14:19

---
+++ plugin/api/Exception.php
@@ -0,0 +1,20 @@
+<?php
+namespace WP_PDF_Builder_Pro\Api;
+
+/**
+ * Exception personnalisée pour l'API PreviewImageAPI
+ */
+class Exception extends \Exception
+{
+    /**
+     * Constructeur
+     *
+     * @param string $message Message d'erreur
+     * @param int $code Code d'erreur
+     * @param \Throwable $previous Exception précédente
+     */
+    public function __construct($message = "", $code = 0, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
