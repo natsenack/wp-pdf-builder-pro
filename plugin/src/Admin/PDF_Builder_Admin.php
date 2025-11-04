8285bdde759bccf19593fc217ed72b77c797ae7d
PDF Builder Pro <pdf-builder-pro@local.dev>
2025-11-04 21:02:04 +0100

    fix: Drag-drop FTP deploy - 2025-11-04 21:02:04

--- plugin/src/Admin/PDF_Builder_Admin.php
+++ plugin/src/Admin/PDF_Builder_Admin.php
@@ -1274,7 +1274,7 @@ class PDF_Builder_Admin {
 
             // Ajouter l'URL de prévisualisation à chaque template
             foreach ($templates as &$template) {
-                $template['preview_url'] = $template_manager->get_template_preview_url($template['name']);
+                $template['preview_url'] = $template_manager->get_template_preview_url($template['id']);
             }
 
             error_log('PDF Builder: ajax_get_builtin_templates - URLs ajoutées, envoi succès');
