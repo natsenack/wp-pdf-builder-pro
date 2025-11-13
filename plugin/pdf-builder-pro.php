013addccf9d0f3ff19f5870a45918e5806af01b0
nastenack <natsenack@gmail.com>
2025-11-13 01:16:33 +0100

    fix: Corrections PHP - 2025-11-13 01:16:33

--- plugin/pdf-builder-pro.php
+++ plugin/pdf-builder-pro.php
@@ -427,6 +427,12 @@ function pdf_builder_add_asset_cache_headers()
 // Gérer les téléchargements PDF en frontend
 add_action('init', 'pdf_builder_handle_pdf_downloads');
 // Charger le plugin pour les requêtes AJAX
+add_action('init', function() {
+    if (isset($_REQUEST['action']) && $_REQUEST['action'] === 'pdf_builder_ajax' && $_SERVER['REQUEST_METHOD'] === 'POST' && is_admin()) {
+        pdf_builder_handle_admin_post_ajax();
+        exit;
+    }
+});
 add_action('admin_init', 'pdf_builder_load_for_ajax');
 add_action('wp_ajax_nopriv_wp_pdf_preview_image', 'pdf_builder_handle_preview_ajax');
 add_action('wp_ajax_wp_pdf_preview_image', 'pdf_builder_handle_preview_ajax');
