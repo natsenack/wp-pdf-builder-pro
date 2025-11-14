c6166368b69a63b59b1de663663a363c735ced6c
nastenack <natsenack@gmail.com>
2025-11-14 18:27:41 +0100

    fix: Corrections PHP - 2025-11-14 18:27:41

--- plugin/templates/admin/templates-page.php
+++ plugin/templates/admin/templates-page.php
@@ -18,7 +18,7 @@ $is_premium = \PDF_Builder\Admin\PdfBuilderAdmin::is_premium_user();
 
 // Créer templates par défaut si aucun template et utilisateur gratuit
 if ($templates_count === 0 && !$is_premium) {
-    \TemplateDefaults::create_default_templates_for_user(get_current_user_id());
+    \PDF_Builder\TemplateDefaults::create_default_templates_for_user(get_current_user_id());
     // Recharger la page pour afficher les templates
     wp_redirect(admin_url('admin.php?page=pdf-builder-templates'));
     exit;
