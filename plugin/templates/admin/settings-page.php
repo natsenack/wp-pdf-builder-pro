b7e9aab360a4f1be44d90e35d962fce79c0d25d4
nastenack <natsenack@gmail.com>
2025-11-17 14:46:57 +0100

    fix: Corrections PHP - 2025-11-17 14:46:57

--- plugin/templates/admin/settings-page.php
+++ plugin/templates/admin/settings-page.php
@@ -2863,7 +2863,7 @@
                 </tr>
              </table>
 
-            <div id="dev-license-section" style="<?php echo !isset($settings['developer_enabled']) || !$settings['developer_enabled'] ? 'display: none;' : ''; ?>">
+            <div id="dev-license-section">
             <h3 class="section-title">🔐 Test de Licence</h3>
 
             <table class="form-table">
