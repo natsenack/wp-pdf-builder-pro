ed9ee86d5961f454019b025fc7bfdcb76947d61d
nastenack <natsenack@gmail.com>
2025-11-22 20:24:17 +0100

    fix: Corrections PHP - 2025-11-22 20:24:17

--- plugin/templates/admin/settings-parts/settings-ajax.php
+++ plugin/templates/admin/settings-parts/settings-ajax.php
@@ -840,5 +840,5 @@ function pdf_builder_get_canvas_settings_handler() {
 // Hook AJAX actions - MOVED to pdf-builder-pro.php for global registration
 // add_action('wp_ajax_pdf_builder_clear_cache', 'pdf_builder_clear_cache_handler');
 // add_action('wp_ajax_pdf_builder_save_settings', 'pdf_builder_save_settings_handler');
-// add_action('wp_ajax_pdf_builder_save_canvas_settings', 'pdf_builder_save_canvas_settings_handler');
+add_action('wp_ajax_pdf_builder_save_canvas_settings', 'pdf_builder_save_canvas_settings_handler');
 // add_action('wp_ajax_pdf_builder_get_canvas_settings', 'pdf_builder_get_canvas_settings_handler');
