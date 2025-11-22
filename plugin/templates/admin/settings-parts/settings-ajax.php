ccdb4c3f29ce9edc532aa85e4c1f88349dc4e4e0
nastenack <natsenack@gmail.com>
2025-11-22 16:17:47 +0100

    fix: Corrections PHP - 2025-11-22 16:17:47

--- plugin/templates/admin/settings-parts/settings-ajax.php
+++ plugin/templates/admin/settings-parts/settings-ajax.php
@@ -510,9 +510,14 @@ function pdf_builder_save_canvas_settings_handler() {
 
     error_log('[DEBUG] pdf_builder_save_canvas_settings_handler called');
 
+    // Log all POST data for debugging
+    error_log('[DEBUG] POST data: ' . print_r($_POST, true));
+
     if (wp_verify_nonce($_POST['nonce'], 'pdf_builder_ajax')) {
+        error_log('[DEBUG] Nonce is valid');
 
         $category = sanitize_text_field($_POST['category'] ?? 'dimensions');
+        error_log('[DEBUG] Category: ' . $category);
 
         try {
             $saved_values = [];
@@ -769,6 +774,7 @@ function pdf_builder_save_canvas_settings_handler() {
             send_ajax_response(false, 'Erreur lors de la sauvegarde: ' . $e->getMessage());
         }
     } else {
+        error_log('[DEBUG] Nonce is INVALID');
         send_ajax_response(false, 'Erreur de sécurité - nonce invalide.');
     }
 }// Handler pour récupérer les paramètres canvas
