1297a0c7071017b39dfc340f2c5b476c7118f62d
nastenack <natsenack@gmail.com>
2025-11-24 03:47:33 +0100

    feat: Mise à jour des assets JavaScript/TypeScript - 2025-11-24 03:47:33

--- dev/config/build/webpack.config.js
+++ dev/config/build/webpack.config.js
@@ -59,9 +59,9 @@ module.exports = {
       new TerserPlugin({
         terserOptions: {
           compress: {
-            drop_console: true,  // ✅ ACTIVÉ pour production
+            drop_console: false,  // ✅ TEMPORAIREMENT DÉSACTIVÉ pour debug
             drop_debugger: true,
-            pure_funcs: ['console.log', 'console.info', 'console.debug'] // Supprimer les logs de debug
+            pure_funcs: [] // Ne pas supprimer les logs
           },
           mangle: {
             safari10: true
@@ -69,70 +69,11 @@ module.exports = {
         }
       })
     ],
-    // Activer le code splitting intelligent avec chunks dynamiques
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxSize: 150000, // 150KB max par chunk
-      minSize: 10000,  // 10KB min par chunk
-      cacheGroups: {
-        // Chunk pour React et ses dépendances (si pas externe)
-        react: {
-          test: /[\\/]node_modules[\\/](react|react-dom|react-router)[\\/]/,
-          name: 'react-vendor',
-          chunks: 'all',
-          priority: 20,
-          enforce: true
-        },
-        // Chunk pour les utilitaires lourds
-        heavy: {
-          test: /[\\/]node_modules[\\/](jsbarcode|qrcode|@fortawesome)[\\/]/,
-          name: 'heavy-utils',
-          chunks: 'all',
-          priority: 25,
-          enforce: true
-        },
-        // Chunk pour les composants de l'application
-        components: {
-          test: /[\\/]assets[\\/]js[\\/]pdf-builder-react[\\/]components[\\/]/,
-          name: 'components',
-          chunks: 'async', // Uniquement les imports dynamiques
-          priority: 15,
-          maxSize: 100000, // 100KB max pour les composants
-          minSize: 20000   // 20KB min
-        },
-        // Chunk pour les hooks et contextes
-        hooks: {
-          test: /[\\/]assets[\\/]js[\\/]pdf-builder-react[\\/](hooks|contexts)[\\/]/,
-          name: 'hooks-contexts',
-          chunks: 'all',
-          priority: 12,
-          maxSize: 80000, // 80KB max
-          minSize: 15000  // 15KB min
-        },
-        // Chunk pour les utilitaires
-        utils: {
-          test: /[\\/]assets[\\/]js[\\/]pdf-builder-react[\\/]utils[\\/]/,
-          name: 'utils',
-          chunks: 'all',
-          priority: 10,
-          maxSize: 50000, // 50KB max
-          minSize: 10000  // 10KB min
-        },
-        // Vendor par défaut
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
-          priority: 5
-        }
-      }
-    },
     usedExports: true,
     sideEffects: true
   },
   performance: {
-    hints: 'error', // Plus strict que 'warning'
+    hints: false, // TEMPORAIREMENT DÉSACTIVÉ pour debug
     maxEntrypointSize: 150 * 1024, // 150 KiB (réduit de 200)
     maxAssetSize: 150 * 1024,     // 150 KiB (réduit de 200)
     assetFilter: function(assetFilename) {
