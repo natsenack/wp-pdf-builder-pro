8fb9ea7476e09b73195882a42db9dd974aa7a5a1
nastenack <natsenack@gmail.com>
2025-11-24 14:18:57 +0100

    fix: Corrections PHP - 2025-11-24 14:18:56

--- plugin/templates/admin/settings-parts/settings-templates.php
+++ plugin/templates/admin/settings-parts/settings-templates.php
@@ -409,7 +409,7 @@ if (!empty($current_mappings) && !empty($order_statuses)) {
                         <h4>
                             <?php echo esc_html($status_label); ?>
                             <?php if ($is_custom_status): ?>
-                            <span class="custom-status-indicator" title="Statut personnalisé ajouté par plugin">🔧</span>
+                            <span class="custom-status-indicator" title="Slug détecté et lié au plugin">🔍</span>
                             <?php endif; ?>
                         </h4>
                         <div class="template-selector">
