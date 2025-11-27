98456a2de6cff1ec1e0ea5e07065bf23af367072
nastenack <natsenack@gmail.com>
2025-11-27 01:16:38 +0100

    fix: Corrections PHP - 2025-11-27 01:16:38

--- plugin/templates/admin/settings-parts/settings-scripts.php
+++ plugin/templates/admin/settings-parts/settings-scripts.php
@@ -5,8 +5,5 @@
  * Updated: 2025-11-18 20:20:00
  */
 
-// Inclure d'abord les paramètres canvas
-require_once 'settings-canvas-params.php';
-
 // Les scripts sont maintenant enregistrés dans bootstrap.php pour s'assurer qu'ils sont chargés avant wp_head
 ?>
