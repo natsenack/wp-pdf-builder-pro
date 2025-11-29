8141dce06a7c988d7c23efe7da8914efb5f42741
nastenack <natsenack@gmail.com>
2025-11-29 01:36:22 +0100

    fix: Corrections PHP - 2025-11-29 01:36:22

--- plugin/pdf-builder-pro.php
+++ plugin/pdf-builder-pro.php
@@ -333,7 +333,7 @@ function pdf_builder_ajax_handler_dispatch() {
                 wp_send_json_error('Handler not implemented - use settings-ajax.php');
                 break;
             case 'pdf_builder_view_logs':
-                pdf_builder_view_logs_handler();
+                wp_send_json_error('Handler not implemented - use settings-ajax.php');
                 break;
             case 'pdf_builder_save_template':
                 pdf_builder_save_template_handler();
@@ -2330,27 +2330,27 @@ function pdf_builder_save_all_settings_handler() {
 }
 
 /**
- * Handler pour voir les logs (dispatcher)
+ * Handler pour voir les logs (dispatcher) - REMOVED: Duplicate function, use settings-ajax.php implementation
  */
-function pdf_builder_view_logs_handler() {
-    // Vérifier le nonce
-    if (!wp_verify_nonce($_POST['nonce'], 'pdf_builder_ajax')) {
-        wp_send_json_error('Nonce invalide');
-        return;
-    }
-
-    // Vérifier les permissions
-    if (!current_user_can('manage_options')) {
-        wp_send_json_error('Permissions insuffisantes');
-        return;
-    }
-
-    // Logs simples
-    wp_send_json_success(array(
-        'logs' => array('Aucun log disponible'),
-        'message' => 'Logs récupérés'
-    ));
-}
+// function pdf_builder_view_logs_handler() {
+//     // Vérifier le nonce
+//     if (!wp_verify_nonce($_POST['nonce'], 'pdf_builder_ajax')) {
+//         wp_send_json_error('Nonce invalide');
+//         return;
+//     }
+//
+//     // Vérifier les permissions
+//     if (!current_user_can('manage_options')) {
+//         wp_send_json_error('Permissions insuffisantes');
+//         return;
+//     }
+//
+//     // Logs simples
+//     wp_send_json_success(array(
+//         'logs' => array('Aucun log disponible'),
+//         'message' => 'Logs récupérés'
+//     ));
+// }
 
 /**
  * Vérifier l'état des systèmes avancés
