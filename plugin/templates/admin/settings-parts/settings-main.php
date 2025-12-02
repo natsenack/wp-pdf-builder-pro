59f8474ab8799844676790adbecab569be8b3a51
nastenack <natsenack@gmail.com>
2025-12-02 03:14:50 +0100

    fix: Corrections PHP - 2025-12-02 03:14:50

--- plugin/templates/admin/settings-parts/settings-main.php
+++ plugin/templates/admin/settings-parts/settings-main.php
@@ -139,7 +139,6 @@
                 this.Validator = new SettingsValidator();
                 this.ShapeManager = new SettingsUI();
                 this.SaveManager = new SettingsSaver();
-                this.PreviewManager = new SettingsPreview();
             }
 
             init() {
