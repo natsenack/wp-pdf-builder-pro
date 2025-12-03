0433ffc079ca4e153f40ab299f3ce2bd9205fba9
nastenack <natsenack@gmail.com>
2025-12-03 01:22:15 +0100

    fix: Corrections PHP - 2025-12-03 01:22:15

--- plugin/templates/admin/settings-parts/settings-general.php
+++ plugin/templates/admin/settings-parts/settings-general.php
@@ -43,7 +43,7 @@
                                 <th scope="row"><label for="company_phone_manual">Téléphone</label></th>
                                 <td>
                                     <input type="text" id="company_phone_manual" name="company_phone_manual"
-                                        value="<?php echo esc_attr($company_phone_manual); ?>"
+                                        value="<?php echo esc_attr(get_option('pdf_builder_company_phone_manual', '')); ?>"
                                         placeholder="+33 1 23 45 67 89" />
                                         <p class="description">Téléphone de l'entreprise</p>
                                 </td>
