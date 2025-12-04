3f41b7b6ec2928116a21babd10ac822ef461cd45
nastenack <natsenack@gmail.com>
2025-12-04 19:58:32 +0100

    fix: Corrections PHP - 04/12/2025 19:58:32

--- plugin/pdf-builder-pro.php
+++ plugin/pdf-builder-pro.php
@@ -18,6 +18,7 @@ define('PDF_BUILDER_PLUGIN_DIR', dirname(__FILE__) . '/');
 define('PDF_BUILDER_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('PDF_BUILDER_PRO_ASSETS_URL', plugin_dir_url(__FILE__) . 'assets/');
 define('PDF_BUILDER_VERSION', '1.1.0');
+define('PDF_BUILDER_PRO_VERSION', '1.1.0');
 
 // Premium features constant (set to false for free version)
 if (!defined('PDF_BUILDER_PREMIUM')) {
