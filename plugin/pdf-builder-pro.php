46046fb277dcd058f37409cbb6222cefe2161c92
nastenack <natsenack@gmail.com>
2025-12-30 22:11:16 +0100

    fix: Corrections PHP - 30/12/2025 22:11:16

--- plugin/pdf-builder-pro.php
+++ plugin/pdf-builder-pro.php
@@ -20,7 +20,7 @@
 define('PDF_BUILDER_PLUGIN_FILE', __FILE__);
 define('PDF_BUILDER_PLUGIN_DIR', dirname(__FILE__) . '/');
 define('PDF_BUILDER_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('PDF_BUILDER_PRO_ASSETS_URL', plugin_dir_url(__FILE__) . 'assets/');
+define('PDF_BUILDER_PRO_ASSETS_URL', plugin_dir_url(__FILE__) . 'resources/assets/');
 define('PDF_BUILDER_VERSION', '1.1.0');
 define('PDF_BUILDER_PRO_VERSION', '1.1.0');
 
