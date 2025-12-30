80ab76bbe14ada0e573672a38447edcd2e14a73e
nastenack <natsenack@gmail.com>
2025-12-31 00:44:34 +0100

    feat: Mise à jour des assets JavaScript/TypeScript - 31/12/2025 00:44:34

--- plugin/src/Admin/Managers/SettingsManager.php
+++ plugin/src/Admin/Managers/SettingsManager.php
@@ -10,28 +10,48 @@ namespace PDF_Builder\Admin\Managers;
 use Exception;
 
 /**
- * Classe responsable de la gestion des paramètres
+ * Stub class pour compatibilité - système de cache supprimé
+ */
+class SettingsManager {
+
+    public function __construct($admin) {
+        // Manager désactivé - système de cache supprimé
+    }
+
+    public function registerHooks() {
+        // Hooks désactivés - système de cache supprimé
+    }
+
+    public function saveSettings($settings) {
+        // Sauvegarde désactivée - système de cache supprimé
+        return false;
+    }
+
+    public function getSettings() {
+        // Récupération désactivée - système de cache supprimé
+        return [];
+    }
+}
+
+/**
+ * Classe responsable de la gestion des paramètres - DISABLED
  */
 class SettingsManager_DISABLED
 {
     /**
-     * Instance de la classe principale
+     * Instance de la classe principale - DÉSACTIVÉ
      */
     private $admin;
 
     /**
-     * Constructeur
+     * Constructeur - DÉSACTIVÉ
      */
     public function __construct($admin)
     {
+        // Manager désactivé - système de cache supprimé
         $this->admin = $admin;
-        $this->registerHooks();
     }
-
-    /**
-     * Enregistrer les hooks
-     */
-    private function registerHooks()
+}
     {
         // Hooks pour les paramètres - seulement l'enregistrement, pas la page
         add_action('admin_init', [$this, 'registerSettings']);
