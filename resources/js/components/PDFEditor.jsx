ea2d4dce3ec7e09d59f8e306ef4b541f93b1f607
nastenack <natsenack@gmail.com>
2026-01-02 15:52:47 +0100

    fix: Remove PDFEditor.css to allow CSS React to apply properly

--- resources/js/components/PDFEditor.jsx
+++ resources/js/components/PDFEditor.jsx
@@ -8,7 +8,7 @@ import PropertiesPanel from './PropertiesPanel';
 import TemplateHeader from './TemplateHeader';
 import { SampleDataProvider } from './preview-system/data/SampleDataProvider';
 import { repairProductTableProperties } from '../utils/elementRepairUtils';
-import './PDFEditor.css';
+// import './PDFEditor.css'; // CSS local désactivé - utiliser pdf-builder-react.css à la place
 
 // Fonctions utilitaires pour manipuler les couleurs
 const lightenColor = (color, percent) => {
