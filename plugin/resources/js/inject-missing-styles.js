68189194935a05c98205682e550a1c1e4c9c611d
nastenack <natsenack@gmail.com>
2026-01-02 19:03:58 +0100

    deploy: 02/01/2026 19:03 - 1 fichiers deployes

--- plugin/resources/js/inject-missing-styles.js
+++ plugin/resources/js/inject-missing-styles.js
@@ -108,6 +108,7 @@ function injectMissingStyles() {
   flex-direction: column;
   gap: 0;
   padding: 0;
+  width: 100%;
   background: #ffffff;
   border-bottom: 1px solid #e5e7eb;
   box-shadow: 0 1px 3px rgba(0,0,0,0.05);
