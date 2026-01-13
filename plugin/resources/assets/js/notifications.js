a6c357b534253cf257af2093ba8bd65d0f89c2e5
nastenack <natsenack@gmail.com>
2026-01-13 19:40:29 +0100

    deploy: 13/01/2026 19:40 - 3 fichiers

--- plugin/resources/assets/js/notifications.js
+++ plugin/resources/assets/js/notifications.js
@@ -905,8 +905,12 @@ try {
 
             console.log('[PDF Builder] NOTIFICATIONS.JS - Notice reset:', noticeId);
         }
-    }
 
+    })();
+
+}
+catch (error) {
+    console.error('[PDF Builder] NOTIFICATIONS.JS - Error:', error);
 }
 
 // Force redeploy marker 3
