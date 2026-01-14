b90f20fd2b4c386a252fbcb9cd2d78e89037458f
nastenack <natsenack@gmail.com>
2026-01-14 13:23:58 +0100

    deploy: 14/01/2026 13:23 - 4 fichiers

--- plugin/src/Managers/PDF_Builder_Template_Manager.php
+++ plugin/src/Managers/PDF_Builder_Template_Manager.php
@@ -129,7 +129,7 @@ class PdfBuilderTemplateManager
             // $debugLog('Permission check passed');
 
             // Vérification du nonce
-            if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'pdf_builder_ajax')) {
+            if (!isset($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'pdf_builder_save_template_nonce')) {
                 \wp_send_json_error('Sécurité: Nonce invalide');
                 return;
             }
