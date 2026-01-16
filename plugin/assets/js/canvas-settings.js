3d851f0c7e2ba6af30c62c4792b3db2b748e3299
Dev Team <dev@threeaxe.fr>
2026-01-17 00:25:35 +0100

    deploy: 17/01/2026 00:25 - 7 fichiers

---
+++ plugin/assets/js/canvas-settings.js
@@ -0,0 +1,9 @@
+/**
+ * PDF Builder Canvas Settings JavaScript
+ */
+(function($) {
+    'use strict';
+
+    console.log('[PDF Builder Canvas] Canvas settings initialized');
+
+})(jQuery);
---
+++ plugin/assets/js/settings-main.js
@@ -0,0 +1,9 @@
+/**
+ * PDF Builder Settings Main JavaScript
+ */
+(function($) {
+    'use strict';
+
+    console.log('[PDF Builder Settings] Main settings initialized');
+
+})(jQuery);
