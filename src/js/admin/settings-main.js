8f58ef72c221e33e949c0c0ac0c4f8ddcd07a7f7
Dev Team <dev@threeaxe.fr>
2026-01-17 20:06:26 +0100

    deploy: 17/01/2026 20:06 - 14 fichiers

--- src/js/admin/settings-main.js
+++ src/js/admin/settings-main.js
@@ -4,24 +4,34 @@
 (function($) {
     'use strict';
 
-    console.log('[PDF Builder Settings] Main settings initialized');
+    console.log('[PDF Builder Settings] Main settings initialized - checking if on license tab');
 
-    // Initialize modal functionality
-    initializeModals();
+    // Only initialize modal functionality if we're on the license tab
+    var currentUrl = window.location.href;
+    if (currentUrl.indexOf('tab=licence') !== -1) {
+        console.log('[PDF Builder Settings] On license tab - initializing modals');
+        initializeModals();
+    } else {
+        console.log('[PDF Builder Settings] Not on license tab - skipping modal initialization');
+    }
 
     /**
      * Initialize modal functionality
      */
     function initializeModals() {
+        console.log('[PDF Builder Settings] Initializing modal functionality');
+
         // Show deactivate modal when button is clicked
         $(document).on('click', '#deactivate-license-btn', function(e) {
             e.preventDefault();
+            console.log('[PDF Builder Settings] Deactivate button clicked');
             showDeactivateModal();
         });
 
         // Close modal when clicking outside
         $(document).on('click', '.modal-overlay', function(e) {
             if (e.target === this) {
+                console.log('[PDF Builder Settings] Modal overlay clicked - closing modal');
                 $(this).hide();
             }
         });
@@ -29,6 +39,7 @@
         // Close modal with ESC key
         $(document).on('keydown', function(e) {
             if (e.keyCode === 27) { // ESC key
+                console.log('[PDF Builder Settings] ESC key pressed - closing modal');
                 $('.modal-overlay').hide();
             }
         });
