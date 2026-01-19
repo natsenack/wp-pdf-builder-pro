fde90f937d02e5d1476cfc1cebc55eb2ec509129
Dev Team <dev@threeaxe.fr>
2026-01-19 04:02:11 +0100

    deploy: 19/01/2026 04:02 - 14 fichiers

--- plugin/pages/welcome.php
+++ plugin/pages/welcome.php
@@ -11,33 +11,4 @@ if (!current_user_can('manage_options')) {
 // Redirection automatique vers l'éditeur
 wp_redirect(admin_url('admin.php?page=pdf-builder-react-editor'));
 exit;
-    margin-top: 0;
-    color: #0073aa;
-}
-
-.info-column ul {
-    margin: 10px 0;
-    padding-left: 20px;
-}
-
-.info-column li {
-    margin-bottom: 8px;
-    color: #666;
-}
-
-.info-column p {
-    margin: 8px 0;
-    color: #666;
-}
-
-@media (max-width: 768px) {
-    .welcome-info {
-        grid-template-columns: 1fr;
-    }
-    
-    .welcome-header h1 {
-        font-size: 24px;
-    }
-}
-</style>
 
