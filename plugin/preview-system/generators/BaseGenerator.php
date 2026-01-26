206ce33b6982ffdfe74fa480b80f494169392507
Dev Team <dev@threeaxe.fr>
2026-01-26 14:56:15 +0100

    deploy: 26/01/2026 14:56 - 15 fichiers

--- plugin/preview-system/generators/BaseGenerator.php
+++ plugin/preview-system/generators/BaseGenerator.php
@@ -967,7 +967,6 @@ abstract class BaseGenerator
             background-color: white;
             border: 2px solid #ccc;
             margin: 0 auto;
-            padding: 20px;
             box-shadow: 0 0 10px rgba(0,0,0,0.1);
         }
         .canvas-info {
