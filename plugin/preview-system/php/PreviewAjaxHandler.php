c15b1dc60f1af814bbef520a1f77df9c4af945af
Dev Team <dev@threeaxe.fr>
2026-01-26 13:56:41 +0100

    deploy: 26/01/2026 13:56 - 15 fichiers

--- plugin/preview-system/php/PreviewAjaxHandler.php
+++ plugin/preview-system/php/PreviewAjaxHandler.php
@@ -84,11 +84,11 @@ class PreviewAjaxHandler {
     }
 
     public static function generateHtmlPreviewAjax() {
-        // Vérification temporairement moins restrictive pour debug
-        if (!is_user_logged_in()) {
-            wp_send_json_error('Utilisateur non connecté', 403);
-        }
-        
+        // Vérification temporairement désactivée pour debug
+        // if (!is_user_logged_in()) {
+        //     wp_send_json_error('Utilisateur non connecté', 403);
+        // }
+
         $nonce = isset($_POST['nonce']) ? sanitize_text_field($_POST['nonce']) : '';
         if (!wp_verify_nonce($nonce, 'pdf_builder_ajax')) {
             wp_send_json_error('Nonce invalide', 403);
