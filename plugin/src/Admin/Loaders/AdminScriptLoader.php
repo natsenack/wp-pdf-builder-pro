1941300b8034ed69fe99d4950b58c3e29695f2f5
Dev Team <dev@threeaxe.fr>
2026-02-06 18:51:39 +0100

    deploy local: 06/02/2026 18:51 - 13 fichiers

--- plugin/src/Admin/Loaders/AdminScriptLoader.php
+++ plugin/src/Admin/Loaders/AdminScriptLoader.php
@@ -807,6 +807,8 @@ class AdminScriptLoader
         // error_log('[WP AdminScriptLoader] Enqueued pdf-builder-react-init: ' . $init_helper_url);
 
         // React initialization script - initializes PDFBuilderReact component
+        $random_param = '';
+        $nuclear_suffix = '';
         $react_init_url = PDF_BUILDER_PLUGIN_URL . 'assets/js/pdf-builder-react-init.min.js' . $random_param;
         \wp_enqueue_script('pdf-builder-react-initializer', $react_init_url, ['pdf-builder-react-main'], $version_param . $nuclear_suffix, true);
         wp_script_add_data('pdf-builder-react-initializer', 'type', 'text/javascript');
